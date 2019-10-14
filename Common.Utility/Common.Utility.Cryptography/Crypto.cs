67814989fe5c25c61f772ed9a50ee4b7f2b157f3
HiroHsu <hiro@icando.today>
2019-10-14 14:40:02 +0800

    #新增 MD5編碼方法 #新增 MD5編碼方法單元測試

---
+++ Common.Utility/Common.Utility.Cryptography/Crypto.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Common.Utility.Cryptography
+{
+    public static class Crypto
+    {
+        /// <summary>
+        /// 將一字串編碼成MD5
+        /// </summary>
+        /// <param name="str">字串</param>
+        /// <returns>編碼後的MD5字串</returns>
+        public static string MD5Hash(this string str)
+        {
+            var md5 = MD5.Create();//建立一個MD5
+            byte[] source = Encoding.Default.GetBytes(str);//將字串轉為Byte[]
+            byte[] crypto = md5.ComputeHash(source);//進行MD5加密
+            StringBuilder hash = new StringBuilder();
+            for (int i = 0; i < crypto.Length; i++)
+            {
+                hash.Append(crypto[i].ToString("x2"));//把加密後的字串從Byte[]轉為字串
+            }
+            return hash.ToString();
+
+        }
+    }
+}
---
+++ Common.Utility/Common.Utility.CryptographyTests/CryptoTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Common.Utility.Cryptography;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Utility.Cryptography.Tests
+{
+    [TestFixture()]
+    public class CryptoTests
+    {
+        [TestCase("12345ABCDE", ExpectedResult = "fc85a7ce091aea86ef3463b9166e9b06")]
+        
+        public string MD5HashTest(string words)
+        {
+            return words.MD5Hash();
+        }
+    }
+}
