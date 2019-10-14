2700ce96cbd1f6034e78af90bda01e720d70ee37
HiroHsu <hiro@icando.today>
2019-10-14 14:18:14 +0800

    #新增 月份差異，只計算月份不算天數 #新增 月份差異單元測試

---
+++ Common.Utility/Common.Utility.Difference/DateDifference.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Common.Utility.Difference
+{
+    public static class DateDifference
+    {
+        /// <summary>
+        /// 計算兩個日期之間的月份數
+        /// </summary>
+        /// <param name="StartDate">比對基準日期</param>
+        /// <param name="EndDate">比對差異目標日期</param>
+        /// <returns></returns>
+        public static int MonthsDifference(this DateTime StartDate, DateTime EndDate)
+        {
+            //                        24216     +      5         -      24216 + 4
+            int totalMonth = (EndDate.Year * 12 + EndDate.Month) - (StartDate.Year * 12 + StartDate.Month);
+
+            return totalMonth;
+
+        }
+    }
+}
---
+++ Common.Utility/Common.Utility.DifferenceTests/DateDifferenceTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Common.Utility.Difference;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Utility.Difference.Tests
+{
+    [TestFixture()]
+    public class DateDifferenceTests
+    {
+        [TestCase(1925, 11, 15, 1925, 11, 25, ExpectedResult = 0)]
+        [TestCase(1925, 12, 01, 1926, 1, 30, ExpectedResult = 1)]
+
+        public int MonthsDifferenceTest(int startYear, int startMonth, int startDay, int endYear, int endMonth, int endDay)
+        {
+            var startDate = new DateTime(startYear, startMonth, startDay);
+            var endDate = new DateTime(endYear, endMonth, endDay);
+            var result = startDate.MonthsDifference(endDate);
+            return result;
+        }
+    }
+}
